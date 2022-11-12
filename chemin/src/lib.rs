3a42b70f9caefeddb7495a45f311172e64f65db3
Mahdrentys <mahdrentys@gmail.com>
2022-11-12 19:10:49 +0100

    Create `Chemin` trait

--- chemin/src/lib.rs
+++ chemin/src/lib.rs
@@ -1,1 +1,6 @@
+pub type Locale<'a> = &'a str;
 
+pub trait Chemin: Sized {
+    fn parse(url: &str, accepted_locales: Option<&[Locale]>) -> Option<(Self, Locale<'static>)>;
+    fn generate_url(&self, locale: Locale) -> String;
+}
